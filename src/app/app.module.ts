f6dfec43c069c6876f1ab2fe90ec7d72f9b870a8
amadoran <afamador@espol.edu.ec>
2023-07-12 14:54:36 -0500

    Completo HttpClient Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MastheadComponent } from './masthead/masthead.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectComponent } from './project/project.component';
+import { FooterComponent } from './footer/footer.component';
+
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MastheadComponent,
+    AboutComponent,
+    ProjectComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -17,7 +29,8 @@ import { ServiceWorkerModule } from '@angular/service-worker';
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
