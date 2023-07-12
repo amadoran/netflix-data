541322aaf32f537d876be253a3e82ca0312306d4
amadoran <afamador@espol.edu.ec>
2023-07-12 17:00:56 -0500

    Tabla Con Estilos

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+import { Collection } from '../interfaces/collection';
+import { DatagetterService } from '../servicios/datagetter.service';
+
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.css']
 })
 export class ProjectComponent {
+  public data: Collection[] = [];
+  constructor(private dataProvider: DatagetterService){}
 
+  ngOnInit(){
+    this.dataProvider.getResponse().subscribe(response => {
+      this.data = (response as Collection[]).slice(0, 10)
+    })
+  }
 }
